e992958a62aa86636dbc6dcec11a7bed2c0af891
Balogun <balogunsunday91@gmail.com>
2025-07-30 12:36:58 +0100

    fix: class on useffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import { useState } from "react";
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomePage from "../pages/Home";
+import AboutPage from "../pages/About";
+import Nav from "./components/Navigations";
 function App() {
-  // const [initialValue, setInitialValue] = useState([1, 2, 3, 4, 5, 6, 7]);
-  const [names, setNames] = useState(["Adebayo", "Adeniyi", "Adewale"]);
-  const [name, setName] = useState("");
+
 
   return (
-    <>
-      <div style={{margin: "120px"}}>
-        <Header />
-        <h1>Welcome</h1>
-       {names.map((value, index) => (
-        <h1 key={index}>
-          {value}
-        </h1>
-       )) }
-
-       <input type="text" value={name} label="Name" onChange={(e) => setName(e.target.value)}  /> <br/>
-
-
-       <br/>
-        <button
-          onClick={() => setNames([ ...names, name])}
-        >
-          Add Name
-        </button>
-        <Footer />
-      </div>
-    </>
+    <BrowserRouter>
+    <Nav />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about"  element={<AboutPage />} />
+      
+    </Routes>
+    </BrowserRouter>
   );
 }
 export default App;

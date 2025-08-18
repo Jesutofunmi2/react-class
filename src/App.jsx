2dec6be79e217c33b15d69320dcdd29e1226a404
Balogun <balogunsunday91@gmail.com>
2025-08-18 12:29:34 +0100

    fix: form validation

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,21 @@ import Nav from "./components/Navigations";
 import Song from "./components/Song";
 import Learning from "../pages/Learning";
 import SignUp from "../pages/SignUp";
+import { ClerkProvider } from "@clerk/clerk-react"
 function App() {
-
-
   return (
+    // <ClerkProvider publishableKey={import.meta.env.PUBLIC_CLERK_PUBLISHABLE_KEY}>
     <BrowserRouter>
     <Nav />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about"  element={<AboutPage />} />
-       <Route path="/song/details/:id" element={<Song />} />
-       <Route path="/learning" element={<Learning />} />
-       <Route path="/signup" element={<SignUp />} />
+      <Route path="/song/details/:id" element={<Song />} />
+      <Route path="/learning" element={<Learning />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
     </BrowserRouter>
+    // </ClerkProvider>
   );
 }
 export default App;
